27a98c3c49d40c5c1ac891a436d3246f12b77319
kali <kali@kali.bhairahawa>
2025-04-27 22:50:21 -0700

    Fix: removed the hardcoded naming convention

--- modules/incus/incus-instance/main.tf
+++ modules/incus/incus-instance/main.tf
@@ -1,6 +1,6 @@
 resource "incus_instance" "this" {
   count=var.instance_count
-  name=var.name-"${count.index}"
+  name="${var.name}-${count.index}"
   image=var.image
   profiles=[
     var.profile
