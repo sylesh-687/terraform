54302bc415b586e60e279da4f37d4f2872ddaa0c
kali <kali@kali.bhairahawa>
2025-05-26 05:12:03 -0700

    Fixed the Syntax Errors

--- modules/incus/incus-instance/main.tf
+++ modules/incus/incus-instance/main.tf
@@ -5,11 +5,8 @@ resource "incus_instance" "this" {
   profiles=[
     var.profile
   ]
-  dynamic "config" {
-    for_each = var.config
-    content {
-      "limits.cpu" = config.value["limits"].cpu
-      "limits.memory" = config.value["limits"].memory
-    }
+  config={
+    "limits.cpu" = config.value["limits"].cpu
+    "limits.memory" = config.value["limits"].memory
   }
 }
