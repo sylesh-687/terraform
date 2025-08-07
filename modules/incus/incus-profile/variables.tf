9cf413f96f58fb0c77b530e29f2b3590f7a99567
kali <kali@kali.bhairahawa>
2025-08-07 03:26:09 -0700

    Adding standards

--- modules/incus/incus-profile/variables.tf
+++ modules/incus/incus-profile/variables.tf
@@ -1,32 +1,36 @@
 variable "name" {
+  type        = string
+  description = "Name of the profile"
 }
 
 variable "network" {
-    
+  type        = string
+  description = "Name of the network"
+
 }
 
 variable "devices" {
-    type=map(object({
-        name = string 
-        type = string 
-        properties = map(string)
-    }))
-    default={
-        eth1={
-            name = "eth1"
-            type = "nic"
-            properties = {
-                nictype = "bridged"
-                parent  = "internal"
-            }
-        }
-        root={
-            name = "root"
-            type = "disk"
-            properties = {
-              pool = "default"
-              path = "/"
-            }
-        }
+  type = map(object({
+    name       = string
+    type       = string
+    properties = map(string)
+  }))
+  default = {
+    eth1 = {
+      name = "eth1"
+      type = "nic"
+      properties = {
+        nictype = "bridged"
+        parent  = "internal"
+      }
     }
+    root = {
+      name = "root"
+      type = "disk"
+      properties = {
+        pool = "default"
+        path = "/"
+      }
+    }
+  }
 }
